be24ff8057ddcce86c74165288232ee12ce30bd1
LuckDuracell <lukedru@yahoo.com>
2022-03-30 15:06:39 -0700

    Initial Commit

---
+++ Shared/NuclearatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuclearatorApp.swift
+//  Shared
+//
+//  Created by Luke Drushell on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct NuclearatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
